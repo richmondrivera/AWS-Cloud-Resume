23503693ec79bfa0ec832fa89e88c0073a704521
Richmond Rivera <richmondrivera007@gmail.com>
2026-09-27 03:42:58 +0800

    Configure S3 remote backend for Terraform state

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -11,6 +11,12 @@ terraform {
       version = "~> 2.4"
     }
   }
+
+  backend "s3" {
+    bucket = "richmondrivera-terraform-state"
+    key    = "cloud-resume/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
