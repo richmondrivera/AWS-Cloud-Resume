ff86b87ff5eca6a08fb6f5303585a78acdd1adde
Richmond Rivera <richmondrivera007@gmail.com>
2026-09-27 04:05:49 +0800

    Remove test comment from pipeline trigger test

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -26,6 +26,4 @@ def lambda_handler(event, context):
         },
         'body': json.dumps({'views': int(new_count)})
     }
-# trigger terraform plan test
-# retry terraform plan with new credentials
-# trigger terraform-apply job test
+
